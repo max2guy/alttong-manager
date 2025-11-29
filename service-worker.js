81620b28c35f2f422082ae1d18665bd517265888
max2guy <max2guy@gmail.com>
2025-11-30 00:19:19 +0900

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,60 @@
+const CACHE_NAME = 'alttong-manager-v1';
+const urlsToCache = [
+  './alttong-manager.html',
+  './manifest.json'
+];
+
+// 설치 시 캐시 저장
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('캐시 열기 완료');
+        return cache.addAll(urlsToCache);
+      })
+  );
+  self.skipWaiting();
+});
+
+// 활성화 시 오래된 캐시 삭제
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHE_NAME) {
+            console.log('오래된 캐시 삭제:', cacheName);
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+// 네트워크 요청 가로채기
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request).then(response => {
+          if (!response || response.status !== 200 || response.type !== 'basic') {
+            return response;
+          }
+          const responseToCache = response.clone();
+          caches.open(CACHE_NAME)
+            .then(cache => {
+              cache.put(event.request, responseToCache);
+            });
+          return response;
+        });
+      })
+      .catch(() => {
+        return caches.match('./alttong-manager.html');
+      })
+  );
+});
