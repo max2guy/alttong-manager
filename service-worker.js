297621f7f94f07cc92ee950588cdf146d709d4a7
max2guy <max2guy@gmail.com>
2025-12-24 07:52:43 +0900

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,12 +1,12 @@
-// ★ 캐시 이름 v5.0으로 변경
-const CACHE_NAME = 'alttong-v5.0'; 
+// ★ 캐시 이름 v5.1
+const CACHE_NAME = 'alttong-v5.1'; 
 
 const ASSETS = [
   './',
-  './index.html?v=5.0', // 쿼리스트링 통일
-  './style.css?v=5.0',
-  './app.js?v=5.0',
-  './manifest.json?v=5.0',
+  './index.html?v=5.1', // 쿼리스트링 통일
+  './style.css?v=5.1',
+  './app.js?v=5.1',
+  './manifest.json?v=5.1',
   'https://www.gstatic.com/firebasejs/10.7.1/firebase-app-compat.js',
   'https://www.gstatic.com/firebasejs/10.7.1/firebase-database-compat.js'
 ];
@@ -22,7 +22,6 @@ self.addEventListener('activate', (event) => {
   event.waitUntil(
     caches.keys().then((keys) => Promise.all(
       keys.map((key) => {
-        // v5.0이 아닌 구버전 캐시는 모두 삭제
         if (key !== CACHE_NAME) return caches.delete(key);
       })
     ))
