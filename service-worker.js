2d482763008ffa88904d2732fd0a84be43543986
max2guy <max2guy@gmail.com>
2025-12-24 08:22:14 +0900

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,27 +1,25 @@
-// ★ 캐시 이름 v6.1
-const CACHE_NAME = 'alttong-v6.1';
-
+const CACHE_NAME = 'alttong-v6.0-final';
 const ASSETS = [
   './',
-  './index.html?v=5.1', // 쿼리스트링 통일
-  './style.css?v=5.1',
-  './app.js?v=5.1',
-  './manifest.json?v=5.1',
+  './index.html?v=6.0',
+  './style.css?v=6.0',
+  './app.js?v=6.0',
+  './manifest.json?v=6.0',
   'https://www.gstatic.com/firebasejs/10.7.1/firebase-app-compat.js',
   'https://www.gstatic.com/firebasejs/10.7.1/firebase-database-compat.js'
 ];
 
-self.addEventListener('install', (event) => {
+self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
   );
   self.skipWaiting();
 });
 
-self.addEventListener('activate', (event) => {
+self.addEventListener('activate', event => {
   event.waitUntil(
-    caches.keys().then((keys) => Promise.all(
-      keys.map((key) => {
+    caches.keys().then(keys => Promise.all(
+      keys.map(key => {
         if (key !== CACHE_NAME) return caches.delete(key);
       })
     ))
@@ -29,11 +27,8 @@ self.addEventListener('activate', (event) => {
   self.clients.claim();
 });
 
-self.addEventListener('fetch', (event) => {
+self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
-    })
+    caches.match(event.request).then(response => response || fetch(event.request))
   );
 });
-
